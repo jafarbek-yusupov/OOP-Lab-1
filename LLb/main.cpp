b1f86194e8e27798db2d65fb1244a6d66877bff5
jafarbek-yusupov <j.yusupov@newuu.uz>
2025-01-16 11:31:37 +0500

    Initial Commit

---
+++ LLb/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LLb
+//
+//  Created by Ja�Farbek Yusupov on 16/01/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
